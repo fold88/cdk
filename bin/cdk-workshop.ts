2b4b727c14ff2125f75cc848eb264a34247f7800
Tao Wan <want88@gmail.com>
2018-12-13 01:23:13 -0800

    backup

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -1,24 +1,33 @@
-#!/usr/bin/env node
-import sns = require('@aws-cdk/aws-sns');
-import sqs = require('@aws-cdk/aws-sqs');
+import ec2 = require('@aws-cdk/aws-ec2');
+import rds = require('@aws-cdk/aws-rds');
 import cdk = require('@aws-cdk/cdk');
 
-class CdkWorkshopStack extends cdk.Stack {
+class MyStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeoutSec: 300
+    const vpc = new ec2.VpcNetwork(this, 'VPC');
+
+    new rds.DatabaseCluster(this, 'MyRdsDb', {
+      defaultDatabaseName: 'MyAuroraDatabase',
+      masterUser: {
+        username: 'admin',
+        password: '123456'
+      },
+      engine: rds.DatabaseClusterEngine.Aurora,
+      instanceProps: {
+        instanceType: new ec2.InstanceTypePair(ec2.InstanceClass.Burstable2, ec2.InstanceSize.Small),
+        vpc: vpc,
+        vpcPlacement: {
+          subnetsToUse: ec2.SubnetType.Public
+        }
+      }
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.subscribeQueue(queue);
   }
 }
 
 const app = new cdk.App();
 
-new CdkWorkshopStack(app, 'CdkWorkshopStack');
+new MyStack(app, 'MyStack');
 
 app.run();
